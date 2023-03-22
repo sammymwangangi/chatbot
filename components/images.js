97be7ce93c831e5842da7761f1ae226ccab65b89
Sammy <sammymwinzi2@gmail.com>
2023-03-22 23:32:12 +0300

    integrate chatGPT successfully

---
+++ components/images.js
@@ -0,0 +1,9 @@
+import chatUser from "../public/images/chat-user.svg";
+import chatBot from "../public/images/chat-bot.svg";
+import voice from "../public/images/voice.svg";
+
+export const Images = {
+  chatUser,
+  chatBot,
+  voice,
+};
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import React, { Suspense, Fragment, useRef, useState, useEffect } from "react";
 import axios from "axios";
-import Script from 'next/script'
+import { Images } from "../components/images";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -51,24 +51,6 @@ export default function Home() {
         console.log(error);
       });
   };
-  //collapse
-  // useEffect(() => {
-  //   const coll = document.getElementsByClassName('collapsible');
-
-  //   for (let i = 0; i < coll.length; i++) {
-  //     coll[i].addEventListener('click', function () {
-  //       this.classList.toggle('active');
-
-  //       const content = this.nextElementSibling;
-
-  //       if (content.style.maxHeight) {
-  //         content.style.maxHeight = null;
-  //       } else {
-  //         content.style.maxHeight = content.scrollHeight + 'px';
-  //       }
-  //     });
-  //   }
-  // }, []);
   return (
     <>
       <Head>
@@ -79,69 +61,99 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="chat-bar-collapsible">
-          <button id="chat-button" type="button" className="collapsible">
-            Chat with us!
-            <i
-              id="chat-icon"
-              style={{ color: "#fff" }}
-              className="fa fa-fw fa-comments-o"
-            ></i>
-          </button>
-
-          <div className="content">
-            <div className="full-chat-block">
-              <div className="outer-container">
-                <div className="chat-container">
-                {chatLog.map((message, index) => (
-
-                  <div key={index} id="chatbox">
-                    <h5 id="chat-timestamp"></h5>
-                    <p id="botStarterMessage" className="botText">
-                      <span>Loading...</span>
-                    </p>
-                  </div>
-                ))}
-
-                  <div className="chat-bar-input-block">
-                    <div id="userInput">
-                      <input
-                        id="textInput"
-                        className="input-box"
-                        type="text"
-                        name="msg"
-                        placeholder="Tap 'Enter' to send a message"
-                      />
-                      <p></p>
-                    </div>
-
-                    <div className="chat-bar-icons">
-                      <i
-                        id="chat-icon"
-                        style={{ color: "crimson" }}
-                        className="fa fa-fw fa-heart"
-                        onclick="heartButton()"
-                      ></i>
-                      <i
-                        id="chat-icon"
-                        style={{ color: "#333" }}
-                        className="fa fa-fw fa-send"
-                        onclick="sendButton()"
-                      ></i>
-                    </div>
-                  </div>
-
-                  <div id="chat-bar-bottom">
-                    <p></p>
+        <section className="questions">
+          <div className="questions-working">
+            <div className="questions-header">
+              <h1>Any questions?</h1>
+              <p className="mt-3">Chat with our Award Winning Robo Advisor</p>
+            </div>
+            <div className="row align-items-center mt-5">
+              <div className="col-lg-6 col-md-6 col-sm-12 text-start">
+                <div className="left-voice-img voice-img">
+                  <Image src={Images.voice} className="voice" alt="" />
+                  {/* <Image src={Images.zendesk} alt="" /> */}
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12 text-start">
+                <div className="left-voice-img">
+                  <div className="chat-box-border">
+                    <form onSubmit={handleSubmit}>
+                      <div className="chatbox-main">
+                        <ul>
+                          <li>
+                            <div
+                              className="d-flex align-items-end"
+                              style={{ columnGap: "1.5rem" }}
+                            >
+                              <Image
+                                src={Images.chatUser}
+                                width={64}
+                                height={64}
+                                style={{ position: "relative", zIndex: "99" }}
+                                alt="user"
+                              />
+                              <span>
+                                Hi! How can we help you?
+                              </span>
+                            </div>
+                          </li>
+                          {chatLog.map((message, index) => (
+                            <li key={index}>
+                              <div
+                                className="d-flex align-items-end"
+                                style={{ columnGap: "1.5rem" }}
+                              >
+                                <Image
+                                  src={
+                                    message.type === "user"
+                                      ? Images.chatBot
+                                      : Images.chatUser
+                                  }
+                                  alt={message.type === "user" ? "user" : "bot"}
+                                  width={64}
+                                  height={64}
+                                  style={{ position: "relative", zIndex: "99" }}
+                                />
+                                <div
+                                  key={index}
+                                  className={`tw-flex ${
+                                    message.type === "user"
+                                      ? "tw-justify-end"
+                                      : "tw-justify-start"
+                                  }`}
+                                >
+                                  <div
+                                    className={`${
+                                      message.type === "user"
+                                        ? "tw-bg-[#A276FF]"
+                                        : "tw-bg-[#6943AF1A]"
+                                    } tw-rounded-lg tw-p-4 tw-text-white tw-max-w-sm`}
+                                  >
+                                    {message.message}
+                                  </div>
+                                </div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                        <div className="typing-box d-flex align-items-center gap-3">
+                          <Image src={Images.typeIcon} alt="" />
+                          <input
+                            type="text"
+                            placeholder="Start typing.."
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </main>
-      <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></Script>
-        <Script src="public/vendor/chat.js"></Script>
     </>
   );
 }
